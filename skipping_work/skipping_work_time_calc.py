9ed097bf0e76082ba204d7bf123e7f56a1ffb913
B. Mahmoudian <20880438+babakmahmoudian@users.noreply.github.com>
2021-12-19 14:43:42 +0330

    logging to stdout

--- skipping_work/skipping_work_time_calc.py
+++ skipping_work/skipping_work_time_calc.py
@@ -2,6 +2,7 @@ import skipping_work
 import random
 import time
 from tabulate import tabulate
+import sys
 
 sample_config = {
     "default": {
@@ -54,29 +55,18 @@ if __name__ == '__main__':
         sample_count = sample_config[key]["sample_count"]
         sample_length = sample_config[key]["sample_length"]
 
-        print("Using %s sample data: Count=%d Length=%d" %
-              (key, sample_count, sample_length))
-
-        print("Creating samples...", end=' ', flush=True)
+        sys.stdout.write("Using %s sample data: Count=%d Length=%d\n\n" %
+                         (key, sample_count, sample_length))
 
         samples = create_samples(count=sample_count, length=sample_length)
 
-        print("Done!", flush=True)
-
         solution_times = []
-        print("Calculating process times:")
         for i in range(1, 5):
             solution = getattr(skipping_work, "solution_%d" % i)
-
-            print("Calculating for %s..." %
-                  solution.__doc__, end=' ', flush=True)
-
             solution_times.append(
                 [solution.__doc__, calc_solution_time(solution, samples)]
             )
 
-            print("Done!", flush=True)
-
-        print('\n')
-        print(tabulate(solution_times, headers=['solution', 'time']))
-        print('\n')
+        sys.stdout.write(
+            tabulate(solution_times, headers=['solution', 'time']))
+        sys.stdout.write('\n\n')
