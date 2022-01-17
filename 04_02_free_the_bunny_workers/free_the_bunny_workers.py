235bd076390641d1c47ef5b1159572f6a28548c5
B. Mahmoudian <20880438+babakmahmoudian@users.noreply.github.com>
2022-01-17 12:12:54 +0330

    solution for challenge 04_02 was slightly refactored

--- 04_02_free_the_bunny_workers/free_the_bunny_workers.py
+++ 04_02_free_the_bunny_workers/free_the_bunny_workers.py
@@ -8,21 +8,21 @@ def combination(n, r):
 
 def solution(num_buns, num_required):
     def add_keys(idx_list, start_index, depth, remaining_keys):
-        if depth == 0:
+        if depth == num_required - 1:
             key = remaining_keys.pop(-1)
             for i in range(len(dist)):
                 if i not in idx_list:
                     dist[i].insert(0, key)
             return
 
-        depth -= 1
+        depth += 1
         for i in range(start_index, len(dist)):
             add_keys(idx_list[:] + [i], i + 1, depth, remaining_keys)
 
     key_count = combination(num_buns, num_required - 1)
 
     dist = [[] for i in range(num_buns)]
-    add_keys([], 0, num_required - 1, range(key_count))
+    add_keys([], 0, 0, range(key_count))
 
     return dist
 
