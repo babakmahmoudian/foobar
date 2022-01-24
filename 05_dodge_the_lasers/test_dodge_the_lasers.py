0b0e6672ea77c17e45760bd5f3ae14a191bf3cff
Babak Mahmoudian <babak.mahmoudian@gmail.com>
2022-01-24 13:28:28 +0330

    added the challenge 'dodge the lasers'

---
+++ 05_dodge_the_lasers/test_dodge_the_lasers.py
@@ -0,0 +1,19 @@
+from dodge_the_lasers import solution
+
+
+def test_given_case_1():
+    s = '77'
+    expected = 4208
+
+    actual = solution(s)
+
+    assert actual == expected
+
+
+def test_given_case_2():
+    s = '5'
+    expected = 19
+
+    actual = solution(s)
+
+    assert actual == expected
